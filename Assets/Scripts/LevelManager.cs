1a0a8808c4bd637ec7b0bdb0f1dc6833fac32f16
Standley Eugene <estand25@gmail.com>
2017-03-18 12:01:05 -0400

    Section 7 - up-to 153

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter;
+
+	void Start(){
+		if (autoLoadNextLevelAfter == 0) {
+			Debug.Log ("Level auto load disabled");
+		} else {
+			Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+		}
+	}
+	// Load level for the name scence
+	public void LoadLevel(string name){
+		// Output the current load level
+		Debug.Log("Level load requested for: "+name);
+		
+		// Load the level
+		Application.LoadLevel(name);
+	}
+	
+	// Quit the game
+	public void QuitRequest(){
+		// Output the quit message
+		Debug.Log("I want quit!");
+		
+		// Quit the application
+		Application.Quit();
+	}
+	
+	//Load the next scence based on the build numbers
+	public void LoadNextLevel(){
+		//yield return new WaitForSeconds(fadeTime);
+
+		// Load the next level based on the current load level plus 1
+		Application.LoadLevel(Application.loadedLevel+1);
+		float fadeTime = GameObject.Find ("Image").GetComponent<Fading> ().BeginFade (1);
+	}
+
+}
---
+++ Assets/Scripts/PlayerPrefsManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPrefsManager : MonoBehaviour {
+	const string MASTER_VOLUMNE_KEY = "master_volume";
+	const string DIFFICULT_KEY = "difficult";
+	const string LEVEL_KEY = "level_unlocked_";
+
+	public static void SetMasterVolumne(float volumne){
+		if (volumne >= 0f && volumne <= 1f) {
+			PlayerPrefs.SetFloat (MASTER_VOLUMNE_KEY, volumne);
+		} else {
+			Debug.LogError("Master volumn out of range");
+		}
+	}
+
+	public static float GetMasterVolumne(){
+		return PlayerPrefs.GetFloat (MASTER_VOLUMNE_KEY);
+	}
+
+	public static void UnlockLevel(int level){
+		if (level <= Application.levelCount - 1) {
+			PlayerPrefs.SetInt(LEVEL_KEY + level.ToString(), 1); //use 1 for true
+		} else {
+			Debug.LogError("Trying to unlock not in build order");
+		}
+	}
+
+	public static bool IsLevelUnlocked(int level){
+		int levelValue = PlayerPrefs.GetInt (LEVEL_KEY + level.ToString ());
+		bool isLevelUnlock = (levelValue == 1);
+
+		if (level <= Application.levelCount - 1) {
+			return isLevelUnlock; 
+		}else {
+			Debug.LogError("Trying to get information about a level not in build order");
+		}
+		return false;
+	}
+
+	public static void SetDifficulty(float difficulty){
+		if (difficulty >= 1f && difficulty <= 3f) {
+			PlayerPrefs.SetFloat(DIFFICULT_KEY,difficulty);
+		} else {
+			Debug.LogError("Trying to set difficult that doesn't exist");
+		}
+	}
+
+	public static float GetDifficulty(){
+		return PlayerPrefs.GetFloat(DIFFICULT_KEY);
+	}
+}
