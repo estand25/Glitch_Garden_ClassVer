b665bdb94928480033c3b7adf0ccd2a320362c0a
Standley Eugene <estand25@gmail.com>
2017-03-18 20:21:14 -0400

    Section 7 - Lecture 156

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -7,11 +7,18 @@ public class Attacker : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		Rigidbody2D myRigidbody2d = gameObject.AddComponent<Rigidbody2D> ();
+		myRigidbody2d.isKinematic = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.Translate (Vector3.left * walkSpeed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		if (col) {
+			Debug.Log(name+" triggered enter "+col);
+		}
 	}
 }
