13da84980509104fcb054214df858487cb0b9090
Standley Eugene <estand25@gmail.com>
2017-03-21 21:13:43 -0400

    Section 7 - Lecture 162

---
+++ Assets/Scripts/Projectiles.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectiles : MonoBehaviour {
+	public float Speed;
+	public float Damage;
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.right * Speed * Time.deltaTime);
+	}
+}
