bcfc98673fd1959c489cea14db5f23a1c6defdeb
Standley Eugene <estand25@gmail.com>
2017-03-28 20:34:35 -0400

    Section 7, Lecture 172 1/10
    Started this lecture, but has day time work to do instead.

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour {
+	private Slider timerSlider;
+	public int levelTime;
+
+	// Use this for initialization
+	void Start () {
+		timerSlider = GameObject.FindObjectOfType<Slider> ();
+		timerSlider.value = levelTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timerSlider.value = timerSlider.value - 1;
+	}
+}
---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+	private LevelManager levelmanager;
+
+	void Start(){
+		levelmanager = FindObjectOfType<LevelManager> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		GameObject obj = col.gameObject;
+		
+		if (!obj.GetComponent<Attacker> ()) {
+			return;
+		} else {
+			Destroy(obj);
+			levelmanager.LoadLevel("03b.Lose");
+		}
+	}
+}
